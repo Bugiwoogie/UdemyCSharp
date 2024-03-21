020e37eeb51fa8482e98fdb2381e6ae8e746b210
Ravi <rgajare92@gmail.com>
2024-03-21 15:13:41 +0100

    initialize with gitignoe and a few lessons done
    I did this course before, but it was two years ago, so i decided to redo this course from zero to refresh my knowledge for CSharp

---
+++ 01-ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ten();
+        }
+
+        private static void Ten()
+        {
+            float myFloat = 100.5f;
+            int myInt = 200;
+
+            float result = myFloat + myInt;
+            Console.WriteLine(value: result);
+        }
+
+        private static void Eleven()
+        {
+
+        }
+    }
+}
